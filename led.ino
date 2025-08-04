8539a1029f45b765c02cb7f9c6969335f3317dc9
Jegan9524 <146802247+Jegan9524@users.noreply.github.com>
2025-08-05 00:30:52 +0530

    New

---
+++ led.ino
@@ -0,0 +1,33 @@
+int g=5;
+int r=6;
+int y=7;
+void setup()
+{
+  pinMode(g,OUTPUT);
+  pinMode(r,OUTPUT);
+  pinMode(y,OUTPUT);
+}
+void loop()
+{
+  for(int i=1;i==1;i++)
+  {
+    digitalWrite(g,HIGH);
+    delay(1000);
+    digitalWrite(g,LOW);
+    delay(1000);
+  }
+   for(int i=1;i<=2;i++)
+  {
+    digitalWrite(g,HIGH);
+    delay(1000);
+    digitalWrite(g,LOW);
+    delay(1000);
+  }
+   for(int i=1;i<=3;i++)
+  {
+    digitalWrite(g,HIGH);
+    delay(1000);
+    digitalWrite(g,LOW);
+    delay(1000);
+  }
+}
